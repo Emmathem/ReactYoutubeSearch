ea80ac8f04363e5a23351d85eef7ee8c0b9a3ed7
Falua Temitope Oyewole <emmat0616@gmail.com>
2017-10-19 18:07:51 +0100

    setup work

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss'; 
+
+
+//refractoring complonent to class
+
+//functional based component
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { video: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
+
+
+//Take this component's generated HTML and put it on the page
+//on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));  
